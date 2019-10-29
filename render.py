96c6cf70d76b031ec180df32c20cc358bbf3ebf8
Panu Kalliokoski <panu.kalliokoski@hel.fi>
2019-10-29 22:46:51 +0200

    Fix interpret_note to work on any number

--- render.py
+++ render.py
@@ -1,12 +1,19 @@
 
-notes = ["D", "^D", "E", "=F", "F", "G", "^G", "A", "^A", "B", "c", "^c",
-        "d", "^d", "e", "=f", "f", "g", "^g", "a", "^a", "b", "c'", "^c'"]
+notes = ["=C", "^C",
+        "D", "^D", "E", "=F", "F", "G", "^G", "A", "^A", "B", "=c", "c",
+        "d", "^d", "e", "=f", "f", "g", "^g", "a", "^a", "b"]
 
 def is_bar(note): return note in '|\n'
 
 def interpret_note(note):
     if note is None: return ('z', 1)
-    return (notes[note + 12], 1)
+    if note < -14:
+        pitch, length = interpret_note(note + 12)
+        return (pitch + ',', length)
+    if note >= 10:
+        pitch, length = interpret_note(note - 12)
+        return (pitch + "'", length)
+    return (notes[note + 14], 1)
 
 def reconcile_halves(start, end):
     pitch, length = end[0]
