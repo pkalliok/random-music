64cb8310ac1ad0b7c830fa9eb9c336a3bc40ba16
Panu Kalliokoski <panu.kalliokoski@hel.fi>
2019-10-29 23:56:08 +0200

    Make it possible to seed the melody from command line

--- make_tune.py
+++ make_tune.py
@@ -1,10 +1,16 @@
 
-import render, generate
+import render, generate, sys
 
-def make_tune():
+def make_tune(initial):
     print('X:1\nM:8/16\nK:D')
-    print(render.render_tune(generate.extend(3, 7, [0, None])))
+    print(render.render_tune(generate.extend(3, 7, initial)))
+
+def read_melody(argv):
+    def maybe_int(x):
+        try: return int(x)
+        except ValueError: return None
+    return [maybe_int(x) for x in argv]
 
 if __name__ == '__main__':
-    make_tune()
+    make_tune(read_melody(sys.argv[1:]))
 
