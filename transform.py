f43e9da80ab9409be290adb62f51dd5f0493c362
Panu Kalliokoski <panu.kalliokoski@hel.fi>
2019-10-29 22:47:03 +0200

    Add one_note transform

--- transform.py
+++ transform.py
@@ -2,5 +2,9 @@
 def transposer(amount):
     return (lambda tune: [None if x is None else x + amount for x in tune])
 
-all_transforms = [transposer(5), transposer(-5), transposer(7), transposer(-7)]
+def one_note(tune):
+    return tune[0] + [None] * len(tune - 1)
+
+all_transforms = [transposer(5), transposer(-5), transposer(7), transposer(-7),
+        one_note]
 
