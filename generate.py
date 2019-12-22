ece2f2c1cf5563a396cb248a1c75113b900afd8c
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2019-12-22 22:42:59 +0200

    add target block lengths for all transformations

--- generate.py
+++ generate.py
@@ -4,6 +4,13 @@ from transform import all_transforms
 
 def maybe(n): return 1 == randint(1, n)
 
+def pick_transform(transforms, blocklength):
+    possible = [transform
+            for min_len, max_len, transform in transforms
+            if min_len is None or min_len <= blocklength
+            if max_len is None or max_len >= blocklength]
+    return choice(possible)
+
 def generate_range(chaos, start, end):
     if end - start <= 1 or maybe(chaos): return (start, end)
     middle = int((start+end)/2)
@@ -12,9 +19,8 @@ def generate_range(chaos, start, end):
 
 def apply_random_transform(chaos, tune):
     start, end = generate_range(chaos, 0, len(tune))
-    transform1 = choice(all_transforms)
-    transform2 = choice(all_transforms)
-    return tune[:start] + transform1(transform2(tune[start:end])) + tune[end:]
+    transform = pick_transform(all_transforms, end - start)
+    return tune[:start] + transform(tune[start:end]) + tune[end:]
 
 def transform(selection_chaos, iteration_chaos, tune):
     if maybe(iteration_chaos):
--- transform.py
+++ transform.py
@@ -1,11 +1,8 @@
 
-from random import choice
-
 def transposer(amount):
     return (lambda tune: [None if x is None else x + amount for x in tune])
 
 def one_note(tune):
-    if len(tune) < 1 or len(tune) > 32: return tune
     return [tune[0]] + [None] * (len(tune) - 1)
 
 def repeat(tune):
@@ -13,18 +10,37 @@ def repeat(tune):
     if len(tune) % 2: return repeat(tune[:-1]) + tune[-1:]
     half = int(len(tune)/2)
     start = tune[:half]
-    return start + choice(all_transforms)(start)
+    return start + start # or choice(all_transforms)(start)
 
 def nudger(shift):
-    return (lambda tune: tune[shift:] + tune[:shift])
+    return (lambda tune: tune[-shift:] + tune[:-shift])
 
 def reverser(blocksize):
     return (lambda tune: [note
         for start in reversed(range(0, len(tune), blocksize))
         for note in tune[start:start+blocksize]])
 
-all_transforms = [transposer(5), transposer(-5), transposer(7), transposer(-7),
-    transposer(2), transposer(-2), repeat, repeat, repeat, one_note, one_note,
-    reverser(2), reverser(4), reverser(8), reverser(16),
-    nudger(1), nudger(2), nudger(4)]
+# weight, min_tune_len, max_tune_len, transformation_function
+all_transforms = [
+        (None, None, transposer(5)),
+        (None, None, transposer(-5)),
+        (None, None, transposer(7)),
+        (None, None, transposer(-7)),
+        (None, None, transposer(2)),
+        (None, None, transposer(-2)),
+        (2, 16, repeat),
+        (4, 24, repeat),
+        (8, 32, repeat),
+        (2, 4, one_note),
+        (2, 8, one_note),
+        (2, 12, one_note),
+        (2, 16, one_note),
+        (4, 16, reverser(2)),
+        (8, 32, reverser(4)),
+        (16, 128, reverser(8)),
+        (32, None, reverser(16)),
+        (2, 6, nudger(1)),
+        (4, 12, nudger(2)),
+        (8, 24, nudger(4)),
+    ]
 
